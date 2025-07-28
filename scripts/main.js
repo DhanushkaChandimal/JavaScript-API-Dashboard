cc536432fb49aa63a6a75c30d0e684d1f9e7e3b4
Dhanushka <dhanushkamcr@gmail.com>
2025-07-28 08:59:19 -0500

    Implemented github user card

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 const dogImageContainer = document.getElementById('dog-output');
 const catImageContainer = document.getElementById('cat-output');
+const githubContainer = document.getElementById('github-output');
 
 async function getDogImage() {
     const response = await fetch("https://dog.ceo/api/breeds/image/random");
@@ -28,4 +29,31 @@ async function getCatImage() {
     } else {
         console.error("Failed to fetch cat image");
     }
+}
+
+async function getGitHubUser(){
+    // const response = await fetch("https://api.github.com/");
+    const response = await fetch("https://api.github.com/users/DhanushkaChandimal");
+    const data = await response.json();
+    console.log(data);
+    // console.log(data.avatar_url);
+
+    const gitInnerContainer = document.createElement('div');
+    gitInnerContainer.classList.add('github-inner-container');
+    
+    const avatar = document.createElement('img');
+    avatar.src = data.avatar_url;
+    avatar.alt = "GitHub User Avatar";
+    gitInnerContainer.appendChild(avatar);
+    
+    const username = document.createElement('h2');
+    username.textContent = data.login;
+    gitInnerContainer.appendChild(username);
+    
+    const bio = document.createElement('p');
+    bio.textContent = data.bio || "No bio available";
+    gitInnerContainer.appendChild(bio);
+    
+    githubContainer.innerHTML = ''; // Clear previous images
+    githubContainer.appendChild(gitInnerContainer);
 }
