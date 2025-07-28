b1dddf7388d0c655b564aa0dc1b094ba60a65039
Dhanushka <dhanushkamcr@gmail.com>
2025-07-28 14:30:15 -0500

    Converted weather code to weather description

--- scripts/main.js
+++ scripts/main.js
@@ -11,6 +11,37 @@ const githubNameInput = document.getElementById('github-name-input');
 
 let exchangeRateList;
 
+const weatherCodeToDescription = {
+    0: "Clear sky",
+    1: "Mainly clear, partly cloudy, and overcast",
+    2: "Mainly clear, partly cloudy, and overcast",
+    3: "Mainly clear, partly cloudy, and overcast",
+    45: "Fog and depositing rime fog",
+    48: "Fog and depositing rime fog",
+    51: "Drizzle: Light, moderate, and dense intensity",
+    53: "Drizzle: Light, moderate, and dense intensity",
+    55: "Drizzle: Light, moderate, and dense intensity",
+    56: "Freezing Drizzle: Light and dense intensity",
+    57: "Freezing Drizzle: Light and dense intensity",
+    61: "Rain: Slight, moderate and heavy intensity",
+    63: "Rain: Slight, moderate and heavy intensity",
+    65: "Rain: Slight, moderate and heavy intensity",
+    66: "Freezing Rain: Light and heavy intensity",
+    67: "Freezing Rain: Light and heavy intensity",
+    71: "Snow fall: Slight, moderate, and heavy intensity",
+    73: "Snow fall: Slight, moderate, and heavy intensity",
+    75: "Snow fall: Slight, moderate, and heavy intensity",
+    77: "Snow grains",
+    80: "Rain showers: Slight, moderate, and violent",
+    81: "Rain showers: Slight, moderate, and violent",
+    82: "Rain showers: Slight, moderate, and violent",
+    85: "Snow showers slight and heavy",
+    86: "Snow showers slight and heavy",
+    95: "Thunderstorm: Slight or moderate",
+    96: "Thunderstorm with slight and heavy hail",
+    99: "Thunderstorm with slight and heavy hail"
+}
+
 async function getDogImage() {
     const response = await fetch("https://dog.ceo/api/breeds/image/random");
     const data = await response.json();
@@ -51,7 +82,7 @@ async function getWeather() {
         `Temperature: ${data.current.temperature_2m}°F\n\n` +
         `Relative Humidity: ${data.current.relative_humidity_2m}%\n\n` +
         `Wind Speed: ${data.current.wind_speed_10m} mph\n\n` +
-        `Weather Code: ${data.current.weather_code}`;
+        `Weather: ${weatherCodeToDescription[data.current.weather_code] || "Unknown"}`;
     element.style.whiteSpace = 'pre-line';
     weatherContainer.innerHTML = ''; // Clear previous results
     weatherContainer.appendChild(element);
