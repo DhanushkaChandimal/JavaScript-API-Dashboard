45b2c95b35878087259d2fd7793621d96e290097
Dhanushka <dhanushkamcr@gmail.com>
2025-07-28 08:14:46 -0500

    Implemented getCatImage method to request response from cat API and display image

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 const dogImageContainer = document.getElementById('dog-output');
+const catImageContainer = document.getElementById('cat-output');
 
 async function getDogImage() {
     const response = await fetch("https://dog.ceo/api/breeds/image/random");
@@ -11,5 +12,20 @@ async function getDogImage() {
         dogImageContainer.appendChild(img);
     } else {
         console.error("Failed to fetch dog image:", data.message);
+    }
+}
+
+async function getCatImage() {
+    const response = await fetch("https://api.thecatapi.com/v1/images/search");
+    const data = await response.json();
+    // console.log(data[0]);
+    if (data.length > 0) {
+        const img = document.createElement('img');
+        img.src = data[0].url;
+        img.alt = "Random Cat Image";
+        catImageContainer.innerHTML = ''; // Clear previous images
+        catImageContainer.appendChild(img);
+    } else {
+        console.error("Failed to fetch cat image");
     }
 }
